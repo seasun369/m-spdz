d009bf63dd024e46e684b1979053962fdecfbf92
JackiePrinceWEI <154526098+JackiePrinceWEI@users.noreply.github.com>
2024-05-02 14:35:47 +0800

    Add files via upload

---
+++ online/20Asiacrypt_mpir.h
@@ -0,0 +1,705 @@
+#ifndef Asiacrypt20_SPDZ_H__
+#define Asiacrypt20_SPDZ_H__
+#define NUM_THREADS 8
+
+#include "../network/network.h"
+#include <emp-tool/emp-tool.h>
+#include "utility.h"
+#include "../network/helper.h"
+#include <omp.h>
+#include "../mpir/mpir.h"
+using namespace emp;
+
+template <int nP>
+class Asiacrypt20_SPDZ
+{
+public:
+	mpz_t *a, *b, *c;
+	mpz_t *mac_a, *mac_b, *mac_c;
+	mpz_t key;
+
+	NetIOMP<nP> *io;
+	int party, total_pre, ssp;
+	int mat_sz = 128;
+	int sz = mat_sz * mat_sz;
+	ThreadPool *pool;
+
+	PRP prp;
+	Asiacrypt20_SPDZ(NetIOMP<nP> *io[2], ThreadPool *pool, int party, bool *_delta = nullptr, int ssp = 40)
+	{
+		this->party = party;
+		this->io = io[0];
+		this->ssp = ssp;
+		this->pool = pool;
+
+		a = new mpz_t[sz];
+		b = new mpz_t[sz];
+		c = new mpz_t[sz];
+
+		mac_a = new mpz_t[sz];
+		mac_b = new mpz_t[sz];
+		mac_c = new mpz_t[sz];
+
+		mpz_init(key);
+
+		for (int i = 0; i < sz; i++)
+		{
+			mpz_init(a[i]);
+			mpz_init(b[i]);
+			mpz_init(c[i]);
+			mpz_init(mac_a[i]);
+			mpz_init(mac_b[i]);
+			mpz_init(mac_c[i]);
+		}
+	}
+	~Asiacrypt20_SPDZ()
+	{
+		mpz_clear(key);
+
+		for (int i = 0; i < sz; i++)
+		{
+			mpz_clear(a[i]);
+			mpz_clear(b[i]);
+			mpz_clear(c[i]);
+			mpz_clear(mac_a[i]);
+			mpz_clear(mac_b[i]);
+			mpz_clear(mac_c[i]);
+		}
+		delete[] a;
+		delete[] b;
+		delete[] c;
+		delete[] mac_a;
+		delete[] mac_b;
+		delete[] mac_c;
+	}
+
+	PRG prg;
+
+	// it should be implemented by offline.
+	void get_triple()
+	{
+		std::ifstream fin1, fin2, fin3,fin4;
+		std::ifstream fin_1, fin_2, fin_3;
+		fin1.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/a.txt", std::ios::in);
+		fin2.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/b.txt", std::ios::in);
+		fin3.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/c.txt", std::ios::in);
+		fin4.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/key.txt", std::ios::in);
+		
+
+		fin_1.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/mac_a.txt", std::ios::in);
+		fin_2.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/mac_b.txt", std::ios::in);
+		fin_3.open("/home/jackie/spdz/pre_data/sextuple_Asiacrypt20/128/mac_c.txt", std::ios::in);
+
+		if (!(fin1.is_open() && fin2.is_open() && fin3.is_open() && fin4.is_open() && fin_1.is_open() && fin_2.is_open() && fin_3.is_open()))
+		{
+			std::cerr << "cannot open the file";
+		}
+
+		char line[512000] = {0};
+		std::vector<triple> Triple;
+		while (fin1.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+		
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				for (int j = 0; j < sz; j++){
+					mpz_set_ui(a[j],Triple[i].value[j]);
+				}
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+
+		while (fin2.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				for (int j = 0; j < sz; j++){
+					mpz_set_ui(b[j],Triple[i].value[j]);
+				}
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+		
+		while (fin3.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				for (int j = 0; j < sz; j++){
+					mpz_set_ui(c[j],Triple[i].value[j]);
+				}
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+
+		while (fin4.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				mpz_set_ui(key,Triple[i].value[0]);
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+		
+		while (fin_1.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+		
+		for (int i = 0; i < nP; i++) {
+			if (party == stoi(Triple[i].party)) {
+				for (int j = 0; j < sz; j++) mpz_set_ui(mac_a[j] , Triple[i].value[j]);
+			}
+		}
+		
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+
+		while (fin_2.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				for (int j = 0; j < sz; j++){
+					mpz_set_ui(mac_b[j],Triple[i].value[j]);
+				}
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+
+		while (fin_3.getline(line, sizeof(line)))
+		{
+			triple t;
+			std::stringstream word(line);
+			word >> t.party;
+			uint64_t num;
+			while (word >> num)
+				t.value.push_back(num);
+			Triple.push_back(t);
+		}
+
+		for (int i = 0; i < nP; i++)
+		{
+			if (party == stoi(Triple[i].party))
+			{
+				for (int j = 0; j < sz; j++){
+					mpz_set_ui(mac_c[j],Triple[i].value[j]);
+				}
+			}
+		}
+
+		Triple.clear();
+		memset(line, 0, sizeof(line));
+	}
+
+	void Online_mul(mpz_t *x, mpz_t *y, mpz_t *mac_x, mpz_t *mac_y, mpz_t *output, mpz_t *output_mac)
+	{
+		mpz_t ppp;
+		mpz_init(ppp);
+		mpz_set_ui(ppp, 2305843009213693951UL);
+
+		mpz_t *d = new mpz_t[sz];
+		mpz_t *e = new mpz_t[sz];
+		
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; ++i)
+		{
+			mpz_init(d[i]);
+			mpz_init(e[i]);
+			mpz_sub(d[i],x[i],a[i]);
+			mpz_mod(d[i],d[i],ppp);
+			mpz_sub(e[i],y[i],b[i]);
+			mpz_mod(e[i],e[i],ppp);
+		}
+		
+		uint64_t *d_int;
+		d_int = new uint64_t[sz];
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; i++)
+		{
+			d_int[i]=mpz_get_ui(d[i]);
+		}
+		if (party != 1)
+		{
+			io->send_data(1, d_int, sz * sizeof(uint64_t));
+			io->flush(1);
+			io->recv_data(1, d_int, sz * sizeof(uint64_t));
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_set_ui(d[i],d_int[i]);
+			}
+		}
+		else
+		{
+			uint64_t *tmp[nP + 1];
+			for (int i = 1; i <= nP; ++i)
+			{
+				tmp[i] = new uint64_t[sz];
+			}
+			vector<future<void>> res;
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, tmp, party2]()
+											{ io->recv_data(party2, tmp[party2], sz * sizeof(uint64_t)); }));
+			}
+			joinNclean(res);
+			mpz_t tmp_ji;
+			mpz_init(tmp_ji);
+			for (int i = 0; i < sz; ++i)
+			{
+				for (int j = 2; j <= nP; ++j)
+				{
+					mpz_set_ui(tmp_ji,tmp[j][i]);
+					mpz_add(d[i],d[i],tmp_ji);
+				}
+				mpz_mod(d[i], d[i], ppp);
+			}
+			mpz_clear(tmp_ji);
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; i++)
+			{
+				d_int[i]=mpz_get_ui(d[i]);
+			}
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, d_int, party2]()
+											{
+					io->send_data(party2, d_int, sz * sizeof(uint64_t));
+					io->flush(party2); }));
+			}
+			joinNclean(res);
+			for (int i = 1; i <= nP; ++i)
+			{
+				delete[] tmp[i];
+			}
+		}
+		delete[] d_int;
+		
+		uint64_t *e_int;
+		e_int = new uint64_t[sz];
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; i++)
+		{
+			e_int[i]=mpz_get_ui(e[i]);
+		}
+		if (party != 1)
+		{
+			io->send_data(1, e_int, sz * sizeof(uint64_t));
+			io->flush(1);
+			io->recv_data(1, e_int, sz * sizeof(uint64_t));
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_set_ui(e[i],e_int[i]);
+			}
+		}
+		else
+		{
+			uint64_t *tmp[nP + 1];
+			for (int i = 1; i <= nP; ++i){
+				tmp[i] = new uint64_t[sz];
+			}
+			vector<future<void>> res;
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, tmp, party2]()
+											{ io->recv_data(party2, tmp[party2], sz * sizeof(uint64_t)); }));
+			}
+			joinNclean(res);
+			mpz_t *tmp_ji;
+			tmp_ji = new mpz_t[sz];
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_init(tmp_ji[i]);
+			}
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; ++i)
+			{
+				for (int j = 2; j <= nP; ++j)
+				{
+					mpz_set_ui(tmp_ji[i],tmp[j][i]);
+					mpz_add(e[i],e[i],tmp_ji[i]);
+				}
+				mpz_mod(e[i], e[i], ppp);
+			}
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; i++)
+			{
+				mpz_clear(tmp_ji[i]);
+				e_int[i]=mpz_get_ui(e[i]);
+			}
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, e_int, party2]()
+											{
+					io->send_data(party2, e_int, sz*sizeof(uint64_t));
+					io->flush(party2); }));
+			}
+			joinNclean(res);
+			for (int i = 1; i <= nP; ++i){
+				delete[] tmp[i];
+			}
+		}
+		delete[] e_int;
+
+		mpz_t *db = new mpz_t[sz];
+		mpz_t *ae = new mpz_t[sz];
+		mpz_t *de = new mpz_t[sz];
+		mpz_t *mac_db = new mpz_t[sz];
+		mpz_t *mac_ae = new mpz_t[sz];
+		mpz_t *mac_de = new mpz_t[sz];
+
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i=0;i<sz;i++){
+			mpz_init(db[i]);
+			mpz_init(ae[i]);
+			mpz_init(de[i]);
+			mpz_init(mac_db[i]);
+			mpz_init(mac_ae[i]);
+			mpz_init(mac_de[i]);
+			mpz_set_ui(db[i], 0);
+			mpz_set_ui(ae[i], 0);
+			mpz_set_ui(de[i], 0);
+			mpz_set_ui(mac_db[i], 0);
+			mpz_set_ui(mac_ae[i], 0);
+			mpz_set_ui(mac_de[i], 0);
+		}
+		
+		mpz_t *multc1;
+		multc1 = new mpz_t[sz];
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i=0;i<sz;i++){
+			mpz_init(multc1[i]);
+		}
+		
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i=0;i<mat_sz;i++){
+			for(int j=0;j<mat_sz;j++){
+				int index1 = i * mat_sz + j;
+				for(int k=0;k<mat_sz;k++){
+					mpz_mul(multc1[index1],d[i*mat_sz+k],b[k*mat_sz+j]);
+					mpz_add(db[index1],db[index1],multc1[index1]);
+					mpz_mul(multc1[index1],a[i*mat_sz+k],e[k*mat_sz+j]);
+					mpz_add(ae[index1],ae[index1],multc1[index1]);
+					mpz_mul(multc1[index1],d[i*mat_sz+k],e[k*mat_sz+j]);
+					mpz_add(de[index1],de[index1],multc1[index1]);
+					mpz_mul(multc1[index1],d[i*mat_sz+k],mac_b[k*mat_sz+j]);
+					mpz_add(mac_db[index1],mac_db[index1],multc1[index1]);
+					mpz_mul(multc1[index1],mac_a[i*mat_sz+k],e[k*mat_sz+j]);
+					mpz_add(mac_ae[index1],mac_ae[index1],multc1[index1]);
+					mpz_mul(mac_de[index1],de[index1],key);
+				}
+				mpz_mod(db[index1],db[index1],ppp);
+				mpz_mod(ae[index1],ae[index1],ppp);
+				mpz_mod(de[index1],de[index1],ppp);
+				mpz_mod(mac_db[index1],mac_db[index1],ppp);
+				mpz_mod(mac_ae[index1],mac_ae[index1],ppp);
+				mpz_mod(mac_de[index1],mac_de[index1],ppp);
+			}
+		}
+
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i=0;i<sz;i++){
+			mpz_clear(multc1[i]);
+		}
+		delete[] multc1;
+
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; ++i)
+		{
+			if (party == 1)
+			{
+				mpz_add(output[i],c[i],db[i]);
+				mpz_add(output[i],output[i],ae[i]);
+				mpz_add(output[i],output[i],de[i]);
+				mpz_mod(output[i],output[i],ppp);
+			}
+			else
+			{
+				mpz_add(output[i],c[i],db[i]);
+				mpz_add(output[i],output[i],ae[i]);
+				mpz_mod(output[i],output[i],ppp);
+			}
+			mpz_add(output_mac[i],mac_c[i],mac_db[i]);
+			mpz_add(output_mac[i],output_mac[i],mac_ae[i]);
+			mpz_add(output_mac[i],output_mac[i],mac_de[i]);
+			mpz_mod(output_mac[i],output_mac[i],ppp);
+		}
+
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; ++i)
+		{
+			mpz_clear(d[i]);
+			mpz_clear(e[i]);
+			mpz_clear(db[i]);
+			mpz_clear(ae[i]);
+			mpz_clear(de[i]);
+			mpz_clear(mac_db[i]);
+			mpz_clear(mac_ae[i]);
+			mpz_clear(mac_de[i]);
+		}
+
+		mpz_clear(ppp);
+
+		delete[] d;
+		delete[] e;
+		delete[] db;
+		delete[] ae;
+		delete[] de;
+		delete[] mac_db;
+		delete[] mac_ae;
+		delete[] mac_de;
+	}
+
+	void mac_check(mpz_t *x, mpz_t *mac_x)
+	{
+		mpz_t ppp;
+		mpz_init(ppp);
+		mpz_set_ui(ppp, 2305843009213693951UL);
+
+		uint64_t *x_int;
+		x_int = new uint64_t[sz];
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; i++)
+		{
+			x_int[i] = mpz_get_ui(x[i]);
+		}
+		if (party != 1)
+		{
+			io->send_data(1, x_int, sz * sizeof(uint64_t));
+			io->flush(1);
+			io->recv_data(1, x_int, sz * sizeof(uint64_t));
+			for(int i=0;i<sz;i++){
+				mpz_set_ui(x[i],x_int[i]);
+			}
+		}
+		else
+		{
+			uint64_t *tmp[nP + 1];
+			for (int i = 1; i <= nP; ++i){
+				tmp[i] = new uint64_t[sz];
+			}
+			vector<future<void>> res;
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, tmp, party2]()
+											{ io->recv_data(party2, tmp[party2], sz * sizeof(uint64_t)); }));
+			}
+			joinNclean(res);
+			mpz_t *tmp_ji;
+			tmp_ji = new mpz_t[sz];
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_init(tmp_ji[i]);
+			}
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; ++i){
+				for (int j = 2; j <= nP; ++j){
+					mpz_set_ui(tmp_ji[i],tmp[j][i]);
+					mpz_add(x[i],x[i],tmp_ji[i]);
+				}
+				mpz_mod(x[i], x[i], ppp);
+			}
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; i++)
+			{
+				mpz_clear(tmp_ji[i]);
+				x_int[i]=mpz_get_ui(x[i]);
+			}
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, x_int, party2]()
+											{
+					io->send_data(party2, x_int, sz*sizeof(uint64_t));
+					io->flush(party2); }));
+			}
+			joinNclean(res);
+			for (int i = 1; i <= nP; ++i){
+				delete[] tmp[i];
+			}
+		}
+		delete[] x_int;
+		
+		mpz_t *kk;
+		kk =new mpz_t[sz];
+		mpz_t *sigma_x;
+		sigma_x = new mpz_t[sz];
+		
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i=0;i<sz;i++){
+			mpz_init(sigma_x[i]);
+			mpz_init(kk[i]);
+		}
+		
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for (int i = 0; i < sz; i++){
+			mpz_mul(kk[i],x[i],key);
+			mpz_sub(sigma_x[i],mac_x[i],kk[i]);
+			mpz_mod(sigma_x[i],sigma_x[i],ppp);
+		}
+		for(int i=0;i<sz;i++){
+			mpz_clear(kk[i]);
+		}
+		delete[] kk;
+		
+		uint64_t *sigma_x_int;
+		sigma_x_int = new uint64_t[sz];
+		omp_set_num_threads(NUM_THREADS);
+		#pragma omp parallel for
+		for(int i = 0; i < sz; i++){
+			sigma_x_int[i]=mpz_get_ui(sigma_x[i]);
+		}
+		if (party != 1)
+		{
+			io->send_data(1, sigma_x_int, sz * sizeof(uint64_t));
+			io->flush(1);
+		}
+		else
+		{
+			uint64_t *tmp[nP + 1];
+			for (int i = 1; i <= nP; ++i){
+				tmp[i] = new uint64_t[sz];
+			}
+			vector<future<void>> res;
+			for (int i = 2; i <= nP; ++i)
+			{
+				int party2 = i;
+				res.push_back(pool->enqueue([this, tmp, party2]()
+											{ io->recv_data(party2, tmp[party2], sz * sizeof(uint64_t)); }));
+			}
+			joinNclean(res);
+			uint64_t sum_check = 0;
+			mpz_t *tmp_ji;
+			tmp_ji=new mpz_t[sz];
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_init(tmp_ji[i]);
+			}
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for (int i = 0; i < sz; ++i)
+			{
+				for (int j = 2; j <= nP; ++j){
+					mpz_set_ui(tmp_ji[i],tmp[j][i]);
+					mpz_add(sigma_x[i],sigma_x[i],tmp_ji[i]);
+				}
+				mpz_mod(sigma_x[i], sigma_x[i], ppp);
+				if (mpz_sgn(sigma_x[i]) != 0)
+				{
+					std::cout << "check error" << endl;
+					std::cout << i << endl;
+					sum_check = sum_check + 1;
+				}
+			}
+			if (sum_check == 0)
+				std::cout << "Correct!" << endl;
+			omp_set_num_threads(NUM_THREADS);
+			#pragma omp parallel for
+			for(int i=0;i<sz;i++){
+				mpz_clear(tmp_ji[i]);
+				mpz_clear(sigma_x[i]);
+			}
+			delete[] sigma_x;
+			for (int i = 1; i <= nP; ++i)
+				delete[] tmp[i];
+		}
+		mpz_clear(ppp);
+		delete[] sigma_x_int;
+	}
+};
+#endif // Asiacrypt20_SPDZ_H__
